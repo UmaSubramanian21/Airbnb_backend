d44a659cf6ad5fff266992a05ff69eeedeb3b9a2
UmaSubramanian21 <usmcaofficial@gmail.com>
2025-04-29 15:35:23 +0530

    integrate Razorpay payment API for booking functionality

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const app = express()
+// initialize the instance from Razorpay and crypto
+const Razorpay = require('razorpay')
+const crypto = require('crypto')
 app.use(cors())
+app.use(express.json())
 
 // mongo db connection 
 const db_connection = mongoose.connect('mongodb+srv://umasubramanian2002:C0AsiCJ3QK5h0VpX@airbnb-database.gvm4sug.mongodb.net/airbnb_database?retryWrites=true&w=majority&appName=Airbnb-Database')
@@ -17,15 +21,42 @@ const User_model = mongoose.model("users", {
 }, "users")
 
 User_model.find().then(function (data) {
-    console.log(" model find() works perfectly", data)
+    console.log(" model.find() works perfectly", data)
 }).catch(function (err) {
-    console.log(" model find() doesn't works",err)
+    console.log(" model.find() doesn't work", err)
 })
 
 //sample api for test
 app.get('/api', (req, res) => {
     res.send('api works sucessfully')
 })
+
+// payment method api
+app.post('/payment/api', (req, res) => {
+
+    try {
+        const RazorpayInstance = new Razorpay({
+            key_id: 'rzp_test_8znbbFdn6mpjv8',
+            key_secret: 'AnzLEM4ZHLN8NxPUTvBwDCLd'
+        });
+
+        const options = {
+            amount: req.body.amount * 100,
+            currency: 'INR',
+            receipt: crypto.randomBytes(10).toString('hex')
+        };
+
+        RazorpayInstance.orders.create(options, (err, order) => {
+            if (err) {
+                return res.status(500).send('Order creation failed');
+            }
+            res.status(200).json(order);
+        });
+    } catch (err) {
+        res.status(500).send("Internal server error");
+    }
+});
+
 
 // setting port number
 app.listen(7000, function () {
